e51cf7eadcd479fe48af54efe4783ca0563815ba
collins993 <74765532+collins993@users.noreply.github.com>
2021-05-25 11:55:56 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/rest_movie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rest_movie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/style/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Colors {
+
+  const Colors();
+
+  static const Color mainColor = const Color(0xFFa30303);
+  static const Color secondColor = const Color(0xFF000000);
+  static const Color titleColor = const Color(0xFFd4c9c9);
+  
+}
